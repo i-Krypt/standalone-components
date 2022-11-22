2bfe98fe4c8687ecd2b8747b189cd13ca1a6d547
i-Krypt <vamay505@gmail.com>
2022-11-22 20:42:07 +0300

    changes to backup

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { provideRouter, Routes } from '@angular/router';
+
+const routes = [
+  {
+    path: "feedback-and-chat",
+    loadComponent: () => import('./app/feedback-and-chat/feedback-and-chat.component').then(c => c.FeedbackAndChatComponent),
+  }
+]
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent).catch(err => console.error(err));
+bootstrapApplication(AppComponent, { 
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch(err => console.error(err));
 
 
